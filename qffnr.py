303f1196df73bf1c74e330ba07d8bfd54473525f
Jonáš Schröder <127658687+js-elictes@users.noreply.github.com>
2023-08-25 13:02:28 +0200

    Update qffnr.py

--- qffnr.py
+++ qffnr.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python3
 # Quick Fix File Name Replacement, replaces the first certain text section in all input files in a directory
-# written by Jonáš Schröder in 2023.
 
 import logging
 import os
 
 
-logging.basicConfig(level=logging.INFO)
-
-__version__ = 0.2
-verstr = "QFFNR ver. {}, by Jonáš Schröder".format(__version__)
-print(verstr+"\n"+"-"*len(verstr)+"\n")
-
+# Settings
 file_ending = ".txt" #this is the extension of all files that this will process
 to_be_replaced = "2-0"
 replaced_with = "1-1"
 
+
+#  Configure logging
+logging.basicConfig(level=logging.INFO)
+
+__version__ = "0.3 / 25.08.2023"
+print(f"\n  -- QFFNR {__version__} by Jonáš Schröder --\n")
+
 if __name__ == "__main__":
     selected_files = [f for f in os.listdir() if f.endswith(file_ending)]
     if not selected_files:
-        logging.error(f"{os.getcwd()} does not contain any {file_ending} files")
+        logging.error(f"  {os.getcwd()} -- There are no {file_ending} files\n")
         quit()
-    print(selected_files)
+    print(f"\n{os.getcwd()}\n{selected_files}\n")
 
     for log_file in selected_files:
         with open(log_file, 'r+') as file:
-            logging.info("Processing file {}".format(log_file))
+            logging.info(f"  Processing file  :  {log_file}")
             contents = file.read()
             file.seek(0)
             new_contents = contents.replace(to_be_replaced, replaced_with, 1)
             file.write(new_contents)
             file.truncate()
-    logging.info("Export finished. Normal termination")
+    logging.info("\n  -- finished --\n")
     quit()
